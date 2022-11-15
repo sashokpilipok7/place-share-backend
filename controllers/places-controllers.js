19556726058a04645a6c0f8bab17640f8da772d8
Sasha Pylypenko <sashapylypenko.pov@gmail.com>
2022-11-15 13:53:44 +0100

    feat: image upload

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const { default: mongoose } = require("mongoose");
 const { validationResult } = require("express-validator");
 
@@ -71,12 +72,11 @@ async function createPlace(req, res, next) {
   }
 
   console.log(coords, "2");
-
+  console.log(req);
   const createdPlace = new Place({
     title,
     description,
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/d/d5/%D0%86%D0%BD%D0%B4%D1%83%D1%81%D1%82%D1%80%D1%96%D0%B0%D0%BB%D1%8C%D0%BD%D0%B8%D0%B9_%D0%BA%D0%BE%D0%BB%D0%B5%D0%B4%D0%B6.jpg",
+    imageUrl: req.file.path,
     address,
     location: coords,
     creator,
@@ -160,6 +160,8 @@ async function deletePlace(req, res, next) {
     return next(error);
   }
 
+  const imagePath = place.imageUrl;
+
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
@@ -173,6 +175,8 @@ async function deletePlace(req, res, next) {
     );
     return next(error);
   }
+
+  fs.unlink(imagePath, (err) => console.log(err));
 
   res.status(200).json({ message: "Success!" });
 }
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 
+const fileUpload = require("../middleware/file-upload");
 const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
@@ -11,6 +12,7 @@ router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 router.post(
   "/",
+  fileUpload.single("image"),
   [
     check("title").not().isEmpty(),
     check("description").isLength({ min: 5 }),
